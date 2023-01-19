f4e7c6dfdfdcccb10eeccd6111fdd401033fe84f
猫猫球 <45891684+FanSun01@users.noreply.github.com>
2023-01-19 23:39:33 +0800

    添加项目文件。

---
+++ DailyPoetryM/Services/IPoetryStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using DailyPoetryM.Models;
+
+namespace DailyPoetryM.Services
+{
+    public interface IPoetryStorage
+    {
+        Task InitializeAsync();
+        Task AddAsync(Poetry poetry);
+        Task<Poetry> GetPoetryAsync(int id);
+        Task<IEnumerable<Poetry>> GetPoetryAsync(Expression<Func<Poetry, bool>> where, int skip, int take);
+
+    }
+}
